ada7c8fb9a4b6d29cbc7782c9811ecdd8b0754a7
Dmytro <33005978+Dmytro-Kuzminskyi@users.noreply.github.com>
2018-10-27 18:47:59 +0200

    first commit

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinaryGap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution s = new Solution();
+			Console.Write("Enter N: ");
+			int N = int.Parse(Console.ReadLine());
+			Console.WriteLine("A binary gap of {0}", s.solution(N));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ BinaryGap/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace BinaryGap
+{
+	class Solution
+	{
+		public int solution(int N)
+		{
+			bool isStarted = false;
+			int count = 0;
+			int maxCount = 0;
+			while (N > 0)
+			{
+				if (N % 2 == 0 && !isStarted)
+				{
+					N /= 2;
+					continue;
+				}
+				else
+				{
+					isStarted = true;
+					N /= 2;
+				}
+				if (N % 2 == 0 && isStarted)
+				{
+					count++;
+				}
+				else
+				{
+					maxCount = count > maxCount ? count : maxCount;
+					count = 0;
+				}
+				if (N == 1) break;
+			}
+			return maxCount;
+		}
+	}
+}
